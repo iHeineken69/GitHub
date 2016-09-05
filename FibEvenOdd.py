f4423d75fc68965311eb1468713f72ebef1f8402
Hein Olivier <heinolivier@gmail.com>
2016-09-05 12:05:24 +0200

    added code to the file

---
+++ FibEvenOdd.py
@@ -0,0 +1,39 @@
+def fib(n):
+    results = []
+    a, b = 0, 1
+    while a < int(n):
+        results.append(a)
+        a, b = b, a+b          
+    return results
+
+def even_number(n):
+    results = []
+    a = 0
+    while a < int(n):
+        results.append(a)
+        a = a + 2
+    return results
+
+def odd_number(n):
+    results = []
+    a = 1
+    while a < int(n):
+        results.append(a)
+        a = a + 2
+    return results
+        
+data = int(input("Enter a number for the fib calc: "))
+data1 = int(input("Enter a number for the even_n calc: "))
+data2 = int(input("Enter a number for the odd_n calc: "))
+
+print("This is a fibonacci series")
+print(fib(data))
+print("This is a even number series")
+print(even_number(data1))
+print("This is a odd number series")
+print(odd_number(data2))
+
+a = fib(data)
+b = even_number(data1)
+
+
